0d4598c22786bc086cbfdf366a33b8ce77a23777
anuragjoshiwbs <anurag.joshi@webeesocial.com>
2025-03-26 07:43:04 +0530

    Added route for Startup management in web.php to enable resource handling for startups.

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\PilotController;
 use App\Http\Controllers\Admin\PilotCategoryController;
 use App\Http\Controllers\Admin\PilotUserController;
+use App\Http\Controllers\Admin\StartupController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 
 
@@ -29,6 +30,7 @@ Route::prefix('admin')->middleware('userType:admin')->group(function () {
     Route::resources([
         'seasons' => SeasonController::class,
         'pilots' => PilotController::class,
+        'startups' => StartupController::class,
     ]);
 });
 
